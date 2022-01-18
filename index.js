041adab413bd5c73d3bfe810c9319e1d93b47c66
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-01-18 11:05:40 -0500

    commit tree 50c01f2c3d03c6ac80ec4c66f8a357187b5963d9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const name = "Joe";
 const height = 74;
-const message = `${name} is ${height} inches tall`;
+const message = `$ {name} is {height} inches tall`;
 
 module.exports = {name, height, message};
